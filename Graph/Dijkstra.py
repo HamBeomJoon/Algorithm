921b884ff9522007fbb4b7cc13bfe9c4cef6b882
HamBeomJoon <37996727+HamBeomJoon@users.noreply.github.com>
2021-08-31 12:07:00 +0900

    Update Dijkstra.py

--- Graph/Dijkstra.py
+++ Graph/Dijkstra.py
@@ -1,1 +1,12 @@
-zz
+def dijkstra(start):
+    res = [float('INF') for _ in range(V)]
+    res[start] = 0
+    q = []
+    heapq.heappush(q, (res[start], start))
+    while q:
+        result_x, x = heapq.heappop(q)
+        for fu, fw in graph[x]:
+            if res[fu] > result_x + fw:
+                res[fu] = result_x + fw
+                heapq.heappush(q, ([res[fu], fu]))
+    return res
