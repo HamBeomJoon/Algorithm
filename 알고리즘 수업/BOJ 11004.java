7c560011e5391b2e2176b4a41e8aaf80485e42a0
HamBeomJoon <37996727+HamBeomJoon@users.noreply.github.com>
2021-09-25 17:26:50 +0900

    Create BOJ 11004.java

---
+++ 알고리즘 수업/BOJ 11004.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+public class hw4_QuickSort {
+	static int N, K;
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			arr[i] = num;
+		}
+		
+		quicksort(arr, 0, N-1);
+		System.out.println(arr[K-1]);
+		
+	}
+	public static void quicksort(int[] array, int left, int right) { 
+		if (left >= right) { 
+			return; 
+		} 
+		
+		int pivot = partition(array, left, right);
+		quicksort(array, left, pivot - 1);
+		quicksort(array, pivot, right);
+	}
+	
+	public static int partition(int[] array, int left, int right) {
+		int mid = (left + right) / 2;
+		int pivot = array[mid];
+		 
+		while (left <= right) {
+			while (pivot < array[right]) {
+				right--;
+			}
+			while (pivot > array[left]) {
+				left++;
+		    }
+			if (left <= right) {
+				swap(array, left, right);
+				right--;
+				left++;
+			}
+		}
+		return left;
+	}
+	
+	public static void swap(int[] array, int a, int b) {
+	    int temp = array[a];
+	    array[a] = array[b];
+	    array[b] = temp;
+	}
+
+}
